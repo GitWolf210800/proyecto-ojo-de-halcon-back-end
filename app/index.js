ee3fc887cc16564eb5b54f785ed33510705f84e4
Lobo <ezad821q@hotmail.com>
2025-01-13 09:13:45 -0300

    actualizacion de commits

--- app/index.js
+++ app/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://192.168.3.122:3000',
     credentials: true,              // Permite el uso de cookies
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
